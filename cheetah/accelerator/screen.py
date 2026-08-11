c5ad880fd1ca93c78503eb4984ea43eadc9562b6
jp-ga <97850904+jp-ga@users.noreply.github.com>
2026-08-10 23:30:45 -0700

    delete commented code

--- cheetah/accelerator/screen.py
+++ cheetah/accelerator/screen.py
@@ -291,27 +291,6 @@ class Screen(Element):
             image = dist.log_prob(pos).exp().mT
         elif isinstance(read_beam, ParticleBeam):
             if self.method == "histogram":
-                # # Catch vectorisation, which is currently not supported by
-                # # "histogram"
-                # if (
-                #     len(read_beam.particles.shape) > 2
-                #     or len(read_beam.particle_charges.shape) > 1
-                #     or len(read_beam.energy.shape) > 0
-                # ):
-                #     raise NotImplementedError(
-                #         "The `'histogram'` method of `Screen` does not "
-                #         "support vectorization. Use `'kde'` instead. If "
-                #         "this is a feature you would like to see, please "
-                #         "open an issue on GitHub."
-                #     )
-
-                # image_transposed, _ = torch.histogramdd(
-                #     torch.stack((read_beam.x, read_beam.y)).mT,
-                #     bins=self.pixel_bin_edges,
-                #     weight=read_beam.particle_charges.abs()
-                #     * read_beam.survival_probabilities,
-                # )
-                # image = image_transposed.mT
                 weights = (
                     read_beam.particle_charges.abs() * read_beam.survival_probabilities
                 )
